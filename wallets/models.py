60d141d8427bcdc9572d2465a6cc975caf481781
Aliaksei Liauchuk <headmaster11@yandex.ru>
2020-07-28 11:59:40 +0300

    wip

--- wallets/models.py
+++ wallets/models.py
@@ -5,3 +5,9 @@ from django.contrib.auth.models import User
 class Wallet(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     bts = models.PositiveIntegerField(default=1)
+
+    def save(self, *args, **kwargs):
+        if self.id is None:
+            if Wallet.objects.filter(owner=self.owner).count() >= 10:
+                return False
+        return super(Wallet, self).save(self)
