f719ff617ed62423e1de1fd39b0ebb0e299e9be5
Aliaksei Liauchuk <headmaster11@yandex.ru>
2020-07-28 14:08:36 +0300

    wip

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from django.db.models import Q
 from rest_framework.decorators import api_view
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
@@ -9,18 +10,22 @@ from rest_framework import status
 @api_view(['POST'])
 def register(request):
     data = request.data
+    if User.objects.get(Q(email=data['email']) | Q(username=data['username'])):
+        return Response({'error': 'already exists'}, status.HTTP_400_BAD_REQUEST)
     user = User.objects.create_user(
         username=data['username'],
         email=data['email'],
         password=data['password'],
     )
     token = Token.objects.create(user=user)
-    return Response({'token': token.key}, status=status.HTTP_201_CREATED)
+    return Response({'token': token.key}, status.HTTP_201_CREATED)
 
 
 @api_view(['POST'])
 def login(request):
     data = request.data
+    if not User.objects.get(username=data['username']):
+        return Response(status=status.HTTP_404_NOT_FOUND)
     user = authenticate(username=data['username'], password=data['password'])
     if user:
         token, _ = Token.objects.get_or_create(user=user)
