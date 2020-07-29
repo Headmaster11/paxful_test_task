f153c2dace2a982ea52417d50564458e5643e845
Aliaksei Liauchuk <headmaster11@yandex.ru>
2020-07-29 13:21:03 +0300

    add simple test for wallet

--- paxful_test_task/urls.py
+++ paxful_test_task/urls.py
@@ -12,12 +12,8 @@ from transactions.urls import router as transaction_router
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Paxful test task API",
       default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
--- wallets/tests.py
+++ wallets/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+
+class WalletTest(APITestCase):
+    def setUp(self):
+        user = User.objects.create_user(username='test_user', password='test')
+        res = self.client.post('/login/', {'username': user.username, 'password': 'test'})
+        self.token = res.data['token']
+
+    def test_create_wallet(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        response = self.client.post('/wallets/')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
