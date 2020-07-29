f5329d1ba178efc5c93a810c339a714df0234b80
Aliaksei Liauchuk <headmaster11@yandex.ru>
2020-07-29 12:20:44 +0300

    simple test

--- transactions/tests.py
+++ transactions/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+
+class TransactionTests(APITestCase):
+    def setUp(self):
+        user = User.objects.create(username='test_user')
+        user.set_password('test')
+        user.save()
+        res = self.client.post('/login/', {'username': user.username, 'password': 'test'})
+        print(res)
+
+    def test_create_transaction_unauthorized(self):
+        response = self.client.post('/transactions/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
