a261592fe448178d404866d6b9962ed0dae6231c
Aliaksei Liauchuk <headmaster11@yandex.ru>
2020-07-29 14:28:41 +0300

    make transaction atomic

--- transactions/models.py
+++ transactions/models.py
@@ -1,9 +1,11 @@
-from django.db import models
+from django.db import models, transaction
 from django.forms import ValidationError
 from django.conf import settings
 from django.contrib.auth.models import User
 
 from wallets.models import Wallet
+
+import decimal
 
 
 class Transaction(models.Model):
@@ -12,10 +14,11 @@ class Transaction(models.Model):
     amount = models.DecimalField(max_digits=30, decimal_places=8)
     platform_profit = models.DecimalField(default=0, max_digits=100, decimal_places=15)
 
+    @transaction.atomic
     def save(self, *args, **kwargs):
         if self.pk is None:
             if self.from_wallet.owner != self.to_wallet.owner:
-                self.platform_profit = self.amount * settings.PLATFORM_TRANSFER_CHARGE
+                self.platform_profit = self.amount * decimal.Decimal(settings.PLATFORM_TRANSFER_CHARGE)
             self.from_wallet.bts -= self.amount + self.platform_profit
             if self.from_wallet.bts < 0:
                 raise ValidationError('not enough bts')
