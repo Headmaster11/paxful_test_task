244fb124bbd9653cfc43b07c1ae8b3f36ae4f232
Aliaksei Liauchuk <headmaster11@yandex.ru>
2020-07-29 10:25:13 +0300

    add endpoint for wallet all transactions

--- wallets/views.py
+++ wallets/views.py
@@ -1,12 +1,16 @@
 from django.forms import ValidationError
+from django.db.models import Q
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework import mixins, status
 
 from wallets.models import Wallet
 from wallets.serializers import WalletSerializer
+from transactions.models import Transaction
+from transactions.serializers import TransactionSerializer
 
 
 class WalletViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, GenericViewSet):
@@ -14,6 +18,11 @@ class WalletViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, GenericV
     serializer_class = WalletSerializer
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        if self.action == 'transactions':
+            return Wallet.objects.all()
+        return Wallet.objects.filter(self.request.user)
 
     def create(self, request, *args, **kwargs):
         wallet = WalletSerializer(data=request.data)
@@ -25,3 +34,8 @@ class WalletViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, GenericV
             return Response(wallet.data, status.HTTP_201_CREATED)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True, methods=['GET'])
+    def transactions(self, request, pk):
+        data = Transaction.objects.filter(Q(from_wallet__pk=pk) | Q(to_wallet__pk=pk))
+        return Response(TransactionSerializer(data, many=True).data)
